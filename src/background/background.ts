5dab4becbc902658c91a81248f4adfe17f57ec4c
Justin V <justin@jhvisser.com>
2025-01-13 15:07:01 -0500

    Adding in ir_patch.jpg automatically overwriting existing downloaded ir_patch.jpg
    Also bump version number

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,3 +1,17 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 });
+
+// Don't prompt for download location/overwrite for ir_patch.jpg
+chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
+  // Only overwrite if this is your "ir_patch.jpg" download
+  if (item.filename.endsWith("ir_patch.jpg")) {
+    suggest({
+      filename: item.filename,
+      conflictAction: "overwrite",
+    });
+  } else {
+    // Leave all other downloads alone
+    suggest();
+  }
+});
